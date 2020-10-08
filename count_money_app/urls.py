706a1e41cc6c2533b40137770ab2f9f153825c42
leongus1 <leongus1@gmail.com>
2020-10-07 22:53:45 -0700

    line history is working but need to make each line be individual

--- count_money_app/urls.py
+++ count_money_app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.count),
+    path('', views.index),
+    path('process_money/', views.count),
+    path('reset/', views.reset),
 ]
--- count_money_app/views.py
+++ count_money_app/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render, HttpResponse, redirect
+import random
 
 # Create your views here.
 
-def count (request):
+def index (request):
+    if 'gold' not in request.session:
+        request.session['gold'] = 0
+        request.session['history']=''
     return render(request, 'index.html')
+
+def count(request):
+    min = int(request.POST['gold_min'])
+    max = int(request.POST['gold_max'])
+    place = request.POST['location']
+    rand = random.randint(min,max)
+    request.session['winloss'] = rand
+    request.session['gold']+=rand
+    if rand > -1:
+        str = (f"Earned {rand} gold from the {place}!\n")
+        request.session['history']  +=str
+    return redirect('/')
+
+def reset(request):
+    request.session['gold'] = 0
+    request.session['history'] =' '
+    return redirect('/')
